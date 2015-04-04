c981cd26375c61f7008d7cdd0ef404cc57ac67da
Rey <quit9909@icloud.com>
2015-04-04 13:46:17 +0900

    初期設定

--- TechMonster/ViewController.swift
+++ TechMonster/ViewController.swift
@@ -11,23 +11,52 @@ import UIKit
 class ViewController: UIViewController {
     
     var timer: NSTimer!
-    var enemyTimer: NStimer!
+    var enemyTimer: NSTimer!
     
     var enemy: Enemy!
-    var pleyer: Player!
+    var player: Player!
     
     let util: TechDraUtility = TechDraUtility()
     
     @IBOutlet var backgroundImageView: UIImageView!
     @IBOutlet var attackButton: UIButton!
     
-    @IBOutlet var 
+    @IBOutlet var enemyImageView: UIImageView!
+    @IBOutlet var playerImageView : UIImageView!
+    
+    @IBOutlet var enemyHPBar: UIProgressView!
+    @IBOutlet var playerHPBar: UIProgressView!
+    
+    @IBOutlet var enemyNameLabel: UILabel!
+    @IBOutlet var playerNameLabel: UILabel!
     
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        enemyHPBar.transform = CGAffineTransformMakeScale(1.0, 4.0)
+        playerHPBar.transform = CGAffineTransformMakeScale(1.0, 4.0)
+        initStatus()
+        
+        enemyTimer = NSTimer.scheduledTimerWithTimeInterval(enemy.speed), target: self, selector: "enemyAttack", userInfo: nil, repeats: true)
+        enemyTimer .fire()
+    }
+    
+    func initStatus() {
+        enemy = Enemy()
+        player = Player()
+        
+        enemyNameLabel.text = enemy.name
+        playerNameLabel.text = player.name
+        
+        enemyImageView.image = enemy.image
+        playerImageView.image = player.image
+        
+        enemyHPBar.progress = enemy.currentHP / enemy.maxHP
+        playerHPBar.progress = player.currentHP / player.maxHP
+        
+        cureHP()
     }
 
     override func didReceiveMemoryWarning() {
